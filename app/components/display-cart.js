f132a5c32705bccd520999f92c983830685e0567
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-02 17:07:41 -0400

    add function to remove selected item from cart

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+
+  actions: {
+    removeFromCart(product) {
+      this.get('shoppingCart').remove(product);
+    }
+
+
+  }
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -1,9 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+
   items: [],
 
   add(item) {
     this.get('items').pushObject(item);
+  },
+
+  remove(item) {
+    // for each item in the itemsarray, find the item that === this particular item
+    //   when true, delete this item in the items array.
+
+    var updated_items = this.get('items');
+
+    for (var i = 0; i < updated_items.length; i++) {
+      if (updated_items[i] === item) {
+        updated_items.splice(i, 1);
+      }
+    }
+    this.set('items', updated_items);
   }
+
 });
+
+
+// the display itself is not updating itself.
+// service not displaying changes - google search

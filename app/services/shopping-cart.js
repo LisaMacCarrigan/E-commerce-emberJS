e0d6a003ba7e42f0da9d74273380ffaec703eb9a
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-02 19:34:22 -0400

    use alternative remove function to remove selected items from cart

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -3,24 +3,30 @@ import Ember from 'ember';
 export default Ember.Service.extend({
 
   items: [],
+  total_cost: null,
 
   add(item) {
     this.get('items').pushObject(item);
   },
 
+  // remove(item) {
+  //   var cart = this.get('items');
+  //
+  //   for (var i = 0; i < cart.length; i++) {
+  //     if (cart[i] === item) {
+  //       cart.splice(i, 1);
+  //     }
+  //   }
+  //   this.set('items', cart);
+  // },
+
   remove(item) {
-    // for each item in the itemsarray, find the item that === this particular item
-    //   when true, delete this item in the items array.
-
-    var updated_items = this.get('items');
-
-    for (var i = 0; i < updated_items.length; i++) {
-      if (updated_items[i] === item) {
-        updated_items.splice(i, 1);
-      }
-    }
-    this.set('items', updated_items);
+    this.get('items').removeObject(item);
   }
+
+  // cost() {
+  //   var
+  // }
 
 });
 

85007b10d1481ab11c6e50d65be65903379114a8
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-11-03 14:54:09 -0400

    add sale route and use firebase query to display on those items that are on sale, include built-in ember helper link-to to go back to main store page

--- app/routes/sale.js
+++ app/routes/sale.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.findAll('product');
+    return this.store.query('product', {
+      orderBy: 'sale',
+      equalTo: true
+    });
   }
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -9,23 +9,11 @@ export default Ember.Service.extend({
     this.get('items').pushObject(item);
   },
 
-  // remove(item) {
-  //   var cart = this.get('items');
-  //
-  //   for (var i = 0; i < cart.length; i++) {
-  //     if (cart[i] === item) {
-  //       cart.splice(i, 1);
-  //     }
-  //   }
-  //   this.set('items', cart);
-  // },
-
   remove(item) {
     this.get('items').removeObject(item);
   },
 
   calcTotal() {
-
     var cart = this.get('items');
     var current_cost = 0;
 
@@ -33,11 +21,6 @@ export default Ember.Service.extend({
       current_cost += item.get('cost');
     });
     this.set('total_cost', current_cost.toFixed(2));
-
   }
 
 });
-
-
-// the display itself is not updating itself.
-// service not displaying changes - google search
